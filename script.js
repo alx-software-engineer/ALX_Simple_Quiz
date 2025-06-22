9aee73dd03e0768b81c6ca4504a9e30e1bbc74c4
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-06-22 07:15:37 +0100

    Implement a simple interactive quiz

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function checkAnswer() {
-    
+    const feedback = document.getElementById("feedback");
+    const correctAnswer = "4";
+
+    // Get User answer
+    const userAnswer = document.querySelector('[name="quiz"]:checked').value;
+
+    // Compare the User’s Answer with the Correct Answer
+    if (userAnswer == correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+
 }
+
+const submitButton = document.getElementById("submit-answer");
+
+submitButton.addEventListener("click", checkAnswer);
